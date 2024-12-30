d64748f3478d46d6b114400fafb7fa92d9121623
elmin007 <imanverdiyevemin414@gmail.com>
2024-12-30 23:21:11 +0400

    Add project files.

---
+++ src/Core/OnionArchApp.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArchApp.Application.Exceptions
+{
+    public class ValidationException:Exception
+    {
+        public ValidationException():this("Validation error occured")
+        {
+            
+        }
+        public ValidationException(String message):base(message) 
+        {
+            
+        }
+        public ValidationException(Exception exception):base(exception.Message)
+        {
+            
+        }
+    }
+}
---
+++ src/Core/OnionArchApp.Application/Interfaces/Repository/IGenericRepositoryAsync.cs
@@ -0,0 +1,17 @@
+﻿using OnionArchApp.Domain.Common;
+using OnionArchApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArchApp.Application.Interfaces.Repository
+{
+    public interface IGenericRepositoryAsync<T> where T : BaseEntity
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync(Guid id);
+        Task<T> AddAsync(T entity); 
+    }
+}
