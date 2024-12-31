19f38b8cb7c820a6acea136d3bad11bbff74797b
elmin007 <imanverdiyevemin414@gmail.com>
2024-12-31 12:12:12 +0400

    Testing an api

---
+++ src/Infrastructure/OnionArchApp.Persistence/Repositories/ProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using OnionArchApp.Application.Interfaces.Repository;
+using OnionArchApp.Domain.Entities;
+using OnionArchApp.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArchApp.Persistence.Repositories
+{
+    public class ProductRepository: GenericRepository<Product>,IProductRepository
+    {
+        public ProductRepository(ApplicationDbContext dbContext):base(dbContext)
+        {
+            
+        }
+    }
+}
---
+++ src/Infrastructure/OnionArchApp.Persistence/ServiceRegistration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using OnionArchApp.Application.Interfaces.Repository;
+using OnionArchApp.Persistence.Context;
+using OnionArchApp.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArchApp.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(opt =>
+            {
+                opt.UseInMemoryDatabase("memoryDb");
+            });
+            services.AddTransient<IProductRepository, ProductRepository>();
+        }
+    }
+}
